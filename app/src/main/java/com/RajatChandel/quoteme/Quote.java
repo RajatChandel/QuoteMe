2eca559cffcc6b125837b9948ab791272cf917b5
RajatChandel <rajat.kumar@mountblue.io>
2019-01-19 17:50:17 +0530

    initial commit

---
+++ app/src/main/java/com/RajatChandel/quoteme/Quote.java
@@ -0,0 +1,11 @@
+package com.RajatChandel.quoteme;
+
+import java.util.Date;
+
+public class Quote {
+
+    private int id;
+    private String content;
+    private String category;
+    private Date date;
+}
